fa1d052d48bfcf736035f131834613860d64059b
brianlinggadjaja <brianlinggadjaja@outlook.com>
2020-11-21 14:13:42 -0800

    Test commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <handle-bar />
-
     <div class="columns">
       <file-storage />
       <router-view></router-view>
@@ -10,12 +8,10 @@
 </template>
 
 <script>
-import HandleBar from './components/HandleBar'
 import FileStorage from './components/FileStorage'
 
 export default {
   components: {
-    HandleBar,
     FileStorage
   }
 }
