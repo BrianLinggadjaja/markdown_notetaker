38b0bc3810bffecab0784462f45614797dd4a9bc
brianlinggadjaja <brianlinggadjaja@outlook.com>
2020-12-01 20:40:46 -0800

    Added example store(state management)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import VueRouter from 'vue-router'
 import routes from './routes'
 import App from './App.vue'
 
+// Import VueX "Page" modules
+import GlobalState from './store/global'
+import FileExplorerState from './store/file-explorer'
+import EditorState from './store/editor'
+import SettingsState from './store/settings'
+
 // Libraries
 import Buefy from 'buefy'
 
@@ -22,14 +28,30 @@ const router = new VueRouter({
   routes
 })
 
+// Updates Vue Router Title on "Page" Change
 router.afterEach((to) => {
   Vue.nextTick(() => {
     document.title = to.meta.title
   })
 })
 
+// Check if development mode and provide debug boolean
+const debug = process.env.NODE_ENV !== 'production'
+
+// Vuex Global Store (State Management) Initalization
+const store = new Vuex.Store({
+  modules: {
+    GlobalState,
+    FileExplorerState,
+    EditorState,
+    SettingsState
+  },
+  strict: debug
+})
+
 // Vue Instance
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
