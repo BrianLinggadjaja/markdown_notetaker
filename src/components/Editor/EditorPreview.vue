82c5ac4589efdf4343a75e8b69c4ff24bc34e5d5
brianlinggadjaja <brianlinggadjaja@outlook.com>
2020-12-02 11:42:57 -0800

    Initalized simple mde library

--- src/components/Editor/EditorPreview.vue
+++ src/components/Editor/EditorPreview.vue
@@ -1,3 +1,29 @@
 <template>
-  <span>Editor/Preview Panel</span>
+  <div>
+    <span>Editor/Preview Panel</span>
+    <textarea id="testEditor"></textarea>
+  </div>
 </template>
+
+<script>
+import SimpleMDE from 'simplemde'
+import '@/scss/libraries/_simplemde.scss'
+
+export default {
+  mounted () {
+    this.initEditor()
+  },
+
+  methods: {
+    initEditor: function () {
+      const test = new SimpleMDE({
+        element: document.getElementById('testEditor'),
+        renderingConfig: {
+          codeSyntaxHighlighting: true
+        }
+      })
+      console.log(test)
+    }
+  }
+}
+</script>
