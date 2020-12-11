38c72523232b8c84e7e4d29f73347766cff06ee0
rs-217 <ronnysusanto68@gmail.com>
2020-12-10 22:51:47 -0800

    used switch component

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -6,10 +6,22 @@
                 <section class="settings pt-0">
                     <h2 class="title is-5 mb-2">Appearance</h2>
                     <div class='buttons'>
-                        <p class="subtitle is-6 ml-2 is-size-8">Change the color scheme<button class="ml-6 is-clickable">Dark Mode</button></p>
+                        <p class="subtitle is-6 ml-2 is-size-8">Change the color scheme<b-switch class="ml-6" v-model="lightMode" passive-type='is-dark' type='is-warning'>
+                        {{ lightMode ? "Light Mode" : "Dark Mode" }}</b-switch>
+                        </p>
                     </div>
                 </section>
             </div>
         </div>
     </main>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            lightMode: true
+        }
+    }
+}
+</script>
