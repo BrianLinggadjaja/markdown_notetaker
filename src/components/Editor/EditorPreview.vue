415f6cd15c83ba2f4d59bcbb4e38cc7b94445241
brianlinggadjaja <brianlinggadjaja@outlook.com>
2020-12-10 23:20:54 -0800

    Linted & Prettified code

--- src/components/Editor/EditorPreview.vue
+++ src/components/Editor/EditorPreview.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
     <b-navbar>
-        <template slot="start">
-            <b-navbar-item>
-                <b-button size="is-small">New</b-button>
-                <b-button size="is-small">Tag</b-button>
-            </b-navbar-item>
-            <b-navbar-item class="buttons is-right">
-                <b-button size="is-small" class="button is-danger">Delete</b-button>
-            </b-navbar-item>
-        </template>
-        <template slot="end">
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                </div>
-            </b-navbar-item>
-        </template>
+      <template slot="start">
+        <b-navbar-item>
+          <b-button size="is-small">New</b-button>
+          <b-button size="is-small">Tag</b-button>
+        </b-navbar-item>
+        <b-navbar-item class="buttons is-right">
+          <b-button size="is-small" class="button is-danger">Delete</b-button>
+        </b-navbar-item>
+      </template>
+      <template slot="end">
+        <b-navbar-item tag="div">
+          <div class="buttons"></div>
+        </b-navbar-item>
+      </template>
     </b-navbar>
     <textarea id="testEditor"></textarea>
   </div>
--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,27 +1,34 @@
 <template>
-    <main class="column is-10">
-        <div class="box">
-            <div class="">
-                <h1 class="title is-3 mb-5">Settings</h1>
-                <section class="settings pt-0">
-                    <h2 class="title is-5 mb-2">Appearance</h2>
-                    <div class='buttons'>
-                        <p class="subtitle is-6 ml-2 is-size-8">Change the color scheme<b-switch class="ml-6" v-model="lightMode" passive-type='is-dark' type='is-warning'>
-                        {{ lightMode ? "Light Mode" : "Dark Mode" }}</b-switch>
-                        </p>
-                    </div>
-                </section>
-            </div>
-        </div>
-    </main>
+  <main class="column is-10">
+    <div class="box">
+      <div class="">
+        <h1 class="title is-3 mb-5">Settings</h1>
+        <section class="settings pt-0">
+          <h2 class="title is-5 mb-2">Appearance</h2>
+          <div class="buttons">
+            <p class="subtitle is-6 ml-2 is-size-8">
+              Change the color scheme<b-switch
+                class="ml-6"
+                v-model="lightMode"
+                passive-type="is-dark"
+                type="is-warning"
+              >
+                {{ lightMode ? 'Light Mode' : 'Dark Mode' }}</b-switch
+              >
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            lightMode: true
-        }
+  data () {
+    return {
+      lightMode: true
     }
+  }
 }
 </script>
