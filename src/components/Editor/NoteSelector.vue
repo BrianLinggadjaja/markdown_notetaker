77be0e70ee7bc1ed8ad71478cf109dcec5753d87
brianlinggadjaja <brianlinggadjaja@outlook.com>
2020-12-11 19:45:58 -0800

    Created Note Card Layout

--- src/components/Editor/NoteSelector.vue
+++ src/components/Editor/NoteSelector.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="note-selector menu column is-primary is-3">
+  <aside class="note-selector menu column is-primary is-4">
     <div class="note-selector__search">
       <b-field>
         <!-- https://buefy.org/documentation/autocomplete -->
@@ -16,8 +16,21 @@
     </div>
 
     <!-- Refactor into new component -->
-    <div>
-      <span>Test</span>
+    <div class="note box">
+      <h1 class="note-title">Comp 484 - Lecture 1 (12/11/2020)</h1>
+      <h2 class="note-book">School</h2>
+      <b-taglist class="note__tag-list">
+        <b-tag type="is-red">First</b-tag>
+        <b-tag type="is-maroon">Second</b-tag>
+        <b-tag type="is-pink">Third</b-tag>
+        <b-tag type="is-purple">Fourth</b-tag>
+        <b-tag type="is-yellow">Fifth</b-tag>
+        <b-tag type="is-orange">Sixth</b-tag>
+        <b-tag type="is-lime">Seventh</b-tag>
+        <b-tag type="is-green">Eigth</b-tag>
+        <b-tag type="is-cyan">Ninth</b-tag>
+        <b-tag type="is-blue">Tenth</b-tag>
+      </b-taglist>
     </div>
   </aside>
 </template>
@@ -63,6 +76,17 @@ export default {
       font-size: 0.9rem;
       width: 100%;
     }
+  }
+}
+.note {
+  &-title {
+    font-family: $font-family-header;
+    font-weight: bold;
+    font-size: 1rem;
+  }
+
+  &__tag-list {
+    margin-top: 1rem;
   }
 }
 </style>
